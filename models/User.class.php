2922a8b48a10d5e6109e68e4e6843cba221b12db
dispxxx <contact@dispix.net>
2015-12-07 16:57:59 +0100

    User object

--- models/User.class.php
+++ models/User.class.php
@@ -5,8 +5,8 @@ class User
 
 	// Properties
 	private $id;
-	private $id_adress;
-	private $adress;
+	private $id_address;
+	private $address;
 	private $email;
 	private $hash;
 	private $name;
@@ -25,38 +25,70 @@ class User
 
 
 	// Getters
+
+
+	// Get user id
 	public function getId()
 	{
 		return $this -> id;
 	}
-	public function getIdAdress()
+
+
+	// Get user adress
+	public function getAdress()
 	{
-		return $this -> id_adress;
+		if (!$this -> address)
+		{
+			$addressManager = new AddressManager($this -> db);
+			$this -> address = $addressManager -> readById($this -> id_address);
+		}
+		return $this -> address;
 	}
+
+
+	// Get user email
 	public function getEmail()
 	{
 		return $this -> email;
 	}
+
+
+	// Get user hash
 	public function getHash()
 	{
 		return $this -> hash;
 	}
+
+
+	// Get user name
 	public function getName()
 	{
 		return $this -> name;
 	}
+
+
+	// Get user surname
 	public function getSurname()
 	{
 		return $this -> surname;
 	}
+
+
+	// Get user status
 	public function getStatus()
 	{
 		return $this -> status;
 	}
+
+
+	// Get user registration date
 	public function getDateRegistration()
 	{
 		return $this -> date_registration;
 	}
+
+
+	// Get user last connection date
 	public function getDateConnection()
 	{
 		return $this -> date_connection;
@@ -64,8 +96,140 @@ class User
 
 
 	// Setters
+
+
+	// Set user adress
 	public function setAdress(Adress $adress)
 	{
+		$this -> id_adress = $adress -> getId();
+		return true;
+	}
 
+
+	// Set user email
+	public function setEmail($email)
+	{
+		if (strlen($email) > 5 && strlen($email) < 63)
+		{
+			if (preg_match("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-zA-Z]{2,5}$#", $email))
+			{
+				$this ->email = $email;
+				return true;
+			}
+			else
+			{
+				throw new Exception('Email format invalid');
+			}
+		}
+		else
+		{
+			throw new Exception('Email format invalid');
+		}
+	}
+
+
+	// Set user hash
+	public function setHash($password, $password2)
+	{
+		if (strlen($password) > 7 && strlen($password) < 32)
+		{
+			if ($password == $password2)
+			{
+				$this ->password = password_hash($password, PASSWORD_DEFAULT);
+				return true;
+			}
+			else
+			{
+				throw new Exception("Passwords don't match");
+			}
+		}
+		else
+		{
+			throw new Exception('Password must be between 6 and 31 characters');
+		}
+	}
+
+
+	// Set user name
+	public function setName($name)
+	{
+		if (strlen($login) > 1 && strlen($login) < 32)
+		{
+			if (preg_match("#[a-zA-Z0-9]+[ -_']*$#", $login))
+			{
+				$this ->login = $login;
+				return true;
+			}
+			else
+			{
+				throw new Exception('Invalid name');
+			}
+		}
+		else
+		{
+			throw new Exception("Name must be between 2 and 31 characters");
+		}
+	}
+
+
+	// Set user surname
+	public function setSurname($surname)
+	{
+		if (strlen($login) > 1 && strlen($login) < 32)
+		{
+			if (preg_match("#[a-zA-Z0-9]+[ -_']*$#", $login))
+			{
+				$this ->login = $login;
+				return true;
+			}
+			else
+			{
+				throw new Exception('Invalid surname');
+			}
+		}
+		else
+		{
+			throw new Exception("Surname must be between 2 and 31 characters");
+		}
+	}
+
+
+	// Set user status
+	public function setStatus($status)
+	{
+		if ($status == 0)
+		{
+			$this -> status = 0;
+			return true;
+		}
+		else if ($status == 1)
+		{
+			$this -> status = 1;
+			return true;
+		}
+		else if ($status == 2)
+		{
+			$this -> status = 2;
+			return true;
+		}
+		else
+		{
+			throw new Exception('Invalid status');
+		}
+	}
+
+
+	// Set last connection date
+	public function setDateConnection($date)
+	{
+		if (ctype_digit(strtotime($date)))
+		{
+			$this -> date_connection = $date;
+			return true;
+		}
+		else
+		{
+			throw new Exception('Format needs to be a timestamp');
+		}
 	}
 ?>
--- models/UserManager.class.php
+++ models/UserManager.class.php
@@ -1,218 +1,128 @@
 <?php
 class UserManager
 {
+
+
+	// Properties
 	private $db;
 
+
+	// Constructor
 	public function __construct($db)
 	{
-		$this->db = $db;
+		$this -> db = $db;
 	}
 
-	public function create($login, $password1, $password2, $email, $avatar)
+
+	// Functions
+
+
+	// Create user
+	public function create(Adress $adress, $email, $name, $surname, $password, $password2)
 	{
-		$errors = array();
-		$user = new User();
-		$errors[] = $user->setLogin($login);
-		$errors[] = $user->setPassword($password1, $password2);
-		$errors[] = $user->setEmail($email);
-		$errors[] = $user->setAvatar($avatar);
-		$errors = array_filter($errors, function($val)
+		$user 		= new User($this -> db);
+
+		try
 		{
-			return $val !== true;
-		});
-		if (count($errors) == 0)
+			$user -> setAdress($adress);
+			$user -> setEmail($email);
+			$user -> setName($name);
+			$user -> setSurname($surname);
+			$user -> setHash($password, $password2);
+		}
+		catch (Exception $e)
 		{
-			$login = mysqli_real_escape_string($this->db, $user->getLogin());
-			$email = mysqli_real_escape_string($this->db, $user->getEmail());
-			$password = $user->getHash();
-			$avatar = mysqli_real_escape_string($this->db, $user->getAvatar());
-			$query = "INSERT INTO user (login, password, email, avatar) VALUES('".$login."', '".$password."', '".$email."', '".$avatar."')";
-			$res = mysqli_query($this->db, $query);
+			$err = $e -> getmessage();
+		}
+
+		if (!$err)
+		{
+			$idAdress 	= $user -> getAdress() -> getId();
+			$email 		= $db -> quote($user -> getEmail());
+			$name 		= $db -> quote($user -> getName());
+			$surname 	= $db -> quote($user -> getSurname());
+			$hash 		= $user -> getHash();
+			$query		= '	INSERT INTO user (id_adress, email, name, surname, hash)
+							VALUES ("'.$idAdress.'""'.$email.'","'.$name.'","'.$surname.'","'.$password.'")';
+			$res		= $db -> exec($query);
+
 			if ($res)
 			{
-				$id = mysqli_insert_id($this->db);
+				$id = $db -> lastInsertId();
+
 				if ($id)
 				{
-					return $this->findById($id);
+					return $this -> readById($id);
 				}
 				else
 				{
-					return "Internal server error";
+					throw new Exception('01 : Database error');
 				}
 			}
 			else
 			{
-				return mysqli_error($this->db);
+				throw new Exception('User already exist');
 			}
 		}
 		else
 		{
-			return $errors;
+			throw new Exception($err);
 		}
 	}
-	public function delete(User $user)
+
+
+	// Read all users
+	public function read($n = 0)
 	{
-		$id = $user->getId();
-		$query = "DELETE FROM user WHERE id='".$id."'";
-		$res = mysqli_query($this->db, $query);
-		if ($res)
+		$n = intval($n);
+
+		if ($n > 0)
 		{
-			return true;
+			$query = '	SELECT *
+						FROM user
+						ORDER BY `date_registered` ASC
+						LIMIT '.$n;
 		}
 		else
 		{
-			return "Internal Server Error";
+			$query = '	SELECT *
+						FROM user
+						ORDER BY `date_registered` ASC';
 		}
-	}
-	public function update(User $user)
-	{
-		$id = $user->getId();
-		$login = mysqli_real_escape_string($this->db, $user->getLogin());
-		$password = mysqli_real_escape_string($this->db, $user->getHash());
-		$email = mysqli_real_escape_string($this->db, $user->getEmail());
-		$avatar = mysqli_real_escape_string($this->db, $user->getAvatar());
-		/*/!\*/$query = "UPDATE user SET login='".$login."', password='".$password."', email='".$email."', avatar='".$avatar."' WHERE id='".$id."'";
-		$res = mysqli_query($this->db, $query);
+
+		$res 	= $db -> query($this -> db, $query);
+
 		if ($res)
 		{
-			return $this->findById($id);
+			$users = $res -> fetchAll(PDO::FETCH_CLASS, 'Section', array($this -> db));
 		}
 		else
 		{
-			return "Internal Server Error";
+			throw new Exception('Database error');
 		}
 	}
-	public function find($id)
+
+
+	// Read user by id
+	public function readById($id)
 	{
-		return $this->findById($id);
-	}
-	public function findById($id)
-	{
-		$id = intval($id);
-		$query = "SELECT * FROM user WHERE id='".$id."'";
-		$res = mysqli_query($this->db, $query);
+		$query 	= "SELECT * FROM user WHERE id = '".$id."'";
+		$res 	= $db -> query($query);
+
 		if ($res)
 		{
-			$user = mysqli_fetch_object($res, "User");
-			if ($user)
+			if ($user = $res -> fetchObject('User', array($this -> db)))
 			{
 				return $user;
 			}
 			else
 			{
-				return "User not found";
+				throw new Exception('User not found');
 			}
 		}
 		else
 		{
-			return "Internal Server Error";
+			throw new Exception('Error 02 : Internal server error');
 		}
 	}
-	public function getLast()
-	{
-		$query = "SELECT * FROM user WHERE (UNIX_TIMESTAMP()-UNIX_TIMESTAMP(last))<3 ORDER BY login";
-		$res = mysqli_query($this->db, $query);
-		$listUser = array();
-		while ($user = mysqli_fetch_object($res, 'User'))
-		{
-			$listUser[] = $user;
-		}
-		return $listUser;
-	}
-	public function getLastDate()
-	{
-		if(isset($_SESSION['login']))
-		{
-			$mysql_last_date = mysqli_query("SELECT last_date FROM user WHERE login=".$_SESSION['login']);
-			$reponse_date = mysqli_fetch_assoc($mysql_last_date);
-			$_SESSION["last_date"] = $reponse_date["last_date"];
-			mysqli_query("UPDATE user SET last_date='".date("U")."' WHERE login=".$_SESSION['login']);
-		}
-	}
-	public function findByLogin($login)
-	{
-		if (strlen(trim($login)) > 0)
-		{
-			$login = mysqli_real_escape_string($this->db, $login);
-			$query = "SELECT * FROM user WHERE login='".$login."'";
-			$res = mysqli_query($this->db, $query);
-			if ($res)
-			{
-				$user = mysqli_fetch_object($res, "User");
-				if ($user) {
-					return $user;
-				}
-				else
-					return "User not found";
-			}
-			else
-			{
-				return "Internal Server Error";
-			}
-		}
-		else
-		{
-			return "User not found";
-		}
-	}
-	public function getCurrent()
-	{
-		if (isset($_SESSION['id']))
-		{
-			$query = "SELECT * FROM user WHERE id='".$_SESSION['id']."'";
-			$res = mysqli_query($this->db, $query);
-			if ($res)
-			{
-				$user = mysqli_fetch_object($res, "User");
-				if ($user)
-				{
-					return $user;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else
-			{
-				return false;
-			}
-		}
-		else
-		{
-			return false;
-		}
-	}
-	public function getNbPost($id)
-	{
-		$postManager = new PostManager($this->db);
-		$postList = $postManager->findByIdAuthor($id);
-		if ($postList)
-		{
-			if (is_string($postList))
-			{
-				$errors[] = $postList;
-				return $errors;
-			}
-			else
-			{
-				$postNb = count($postList);
-				if ( $postNb == NULL )
-				{
-					$postNb = 0;
-					return $postNb;
-				}
-				else
-				{
-					return $postNb;
-				}
-			}
-		}
-		else
-		{
-			return "Pas de messages postés";
-		}
-
-	}
-}
 ?>
