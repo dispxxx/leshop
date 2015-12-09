1538955266930313a5c6a220383e5d951d537cb5
Sidi-M <laber.sidim@gmail.com>
2015-12-09 14:41:17 +0100

    un
    deux

--- apps/categories.php
+++ apps/categories.php
@@ -1,3 +1,12 @@
-<?php 
-	require("categories.phtml"); 
- ?>
+<?php
+	$category = new CategoryManager($db);
+	$listCategory = $category->sortByName();
+	$i = 0;
+	while (isset($listCategory[$i]))
+	{
+		$category = $listCategory[$i];
+		require("views/categories.phtml"); 
+		
+		$i++;
+	}
+?>
