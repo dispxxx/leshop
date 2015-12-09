c8848d0710bbeb77d433d8b561c2795da1f95274
Sidi-M <laber.sidim@gmail.com>
2015-12-09 14:02:30 +0100

    un
    deux

--- models/AddressManager.class.php
+++ models/AddressManager.class.php
@@ -65,13 +65,192 @@ class AddressManager
 			}
 		}
 	}
-	public function getDelete(Address $address)
+
+	public function read($n = 0)
 	{
-		$id = $address->getId();
-		$query = "DELETE FROM address WHERE id='".$id."'";
-		$res = $db->exec($query);
-		return "internal Server error"; 
+		$n = intval($n);
+
+		if ($n > 0)
+		{
+			$query = '	SELECT *
+						FROM address
+						ORDER BY `id` ASC
+						LIMIT '.$n;
+		}
+		else
+		{
+			$query = '	SELECT *
+						FROM address
+						ORDER BY `id` ASC';
+		}
+
+		$res 	= $this -> db -> query($query);
+
+		if ($res)
+		{
+			$users = $res -> fetchAll(PDO::FETCH_CLASS, 'Address', array($this -> db));
+			return $users;
+		}
+		else
+		{
+			throw new Exception('Database error');
+		}
 	}
-	
+	public function readById(Address $address)
+	{
+		$idCategory = $category -> getId();
+		$query 		= '	SELECT * FROM address
+						WHERE id="'.$id.'"'; 
+		$res 		= $this -> db -> query($query);
+
+		if ($res)
+		{
+			$items 	= $res -> fetchAll(PDO::FETCH_CLASS, 'Address', array($this -> db));
+			return $address;
+		}
+		else
+		{
+			throw new Exception('Database error');
+		}
+	}
+
+	public function readByStreet($street)
+	{
+		$street = $this -> db -> quote($address -> getStreet());
+		$query 	= 'SELECT * FROM address WHERE street='.$street;
+		$res 	= $this -> db -> query($query);
+
+		if($res)
+		{
+			$address = $res -> fetchObject('Address', array($this -> db));
+
+			if ($address)
+			{
+				return $address;
+			}
+			else
+			{
+				throw new Exception('Address not found');
+			}
+		}
+		else
+		{
+			throw new Exception('Internal Server Error');
+		}
+	}
+
+	public function readByCity($city)
+	{
+		$city = $this -> db -> quote($city -> getCity());
+		$query 	= 'SELECT * FROM address WHERE city='.$city;
+		$res 	= $this -> db -> query($query);
+
+		if($res)
+		{
+			$address = $res -> fetchObject('Address', array($this -> db));
+
+			if ($address)
+			{
+				return $address;
+			}
+			else
+			{
+				throw new Exception('Address not found');
+			}
+		}
+		else
+		{
+			throw new Exception('Internal Server Error');
+		}
+	}
+
+	public function readByCitycode($citycode)
+	{
+		$citycode = $this -> db -> quote($address -> getCitycode());
+		$query 	= 'SELECT * FROM address WHERE citycode='.$citycode;
+		$res 	= $this -> db -> query($query);
+
+		if($res)
+		{
+			$address = $res -> fetchObject('Address', array($this -> db));
+
+			if ($address)
+			{
+				return $address;
+			}
+			else
+			{
+				throw new Exception('Address not found');
+			}
+		}
+		else
+		{
+			throw new Exception('Internal Server Error');
+		}
+	}
+
+	public function readByCountry($country)
+	{
+		$country = $this -> db -> quote($address -> getCountry());
+		$query 	= 'SELECT * FROM address WHERE country='.$country;
+		$res 	= $this -> db -> query($query);
+
+		if($res)
+		{
+			$address = $res -> fetchObject('Address', array($this -> db));
+
+			if ($address)
+			{
+				return $address;
+			}
+			else
+			{
+				throw new Exception('Address not found');
+			}
+		}
+		else
+		{
+			throw new Exception('Internal Server Error');
+		}
+	}
+
+	public function update(Address $address)
+	{
+		$idUser   = $address->getAddress()->getId();
+		$id       = intval($id);
+		$street   = $db->quote($address->getStreet());
+		$city     = $db->quote($address->getCity());
+		$citycode = $db->quote($address->getCitycode());
+		$country  = $db->quote($adress->getCountry());
+		$query    = "UPDATE address SET street ='".$street."', city ='".$city."', citycode='".$citycode."', country='".$country."' WHERE id='".$id."' ";
+		$res      = $db->exec($query);
+		if($res)
+		{
+			$id = $db->lastInsertId();
+			if($id)
+			{
+				return $this->readById($id);
+			}
+			else
+			{
+				throw new Exception('Database error')
+			}
+		}
+	}
+	public function delete(Address $address)
+	{
+		$id    = $address->getId();
+		$query = "DELETE FROM address WHERE id='".$id."'";
+		$res   = $this -> db -> exec($query);
+		if ($res)
+		{
+			return true;
+		}
+		else
+		{
+			throw new Exception('Database error');
+		}
+		
+	}
 }
 ?>
