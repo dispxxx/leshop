9133125f812103fa4f2980a0c0524a6bf4756c84
Hollux <hollux@hotmail.fr>
2015-12-14 11:12:33 +0100

    bullshit

--- apps/basket.php
+++ apps/basket.php
@@ -1,6 +1,8 @@
 <?php 
 	$orderManager = new orderManager($db);
 	$order = $orderManager->readByIdUser($currentUser->getId());
+	$item = $order -> getItemList();
 	var_dump($order);
+	var_dump($item);
 	require("views/basket.phtml");
  ?>
