54cc7f509d90ef12b8053f59b8d4cb962988b620
dispxxx <contact@dispix.net>
2015-12-14 12:02:23 +0100

    Order

--- apps/handler/handler_order.php
+++ apps/handler/handler_order.php
@@ -2,61 +2,58 @@
 if (isset($_GET['page']))
 {
 
-	// Register function
-	if ($_GET['page'] == 'item')
+	// Add item
+	if ($_GET['page'] == 'item' && isset($_GET['id'], $_POST['add_qty']))
 	{
-		if (isset($_GET['id'], $_POST['add_qty']))
+		$qry 			= intval($_POST['add_qty']);
+		$orderManager 	= new OrderManager($db);
+		$itemManager 	= new ItemManager($db);
+
+
+		// Get item
+		try
 		{
-			$qry 			= intval($_POST['add_qty']);
-			$orderManager 	= new OrderManager($db);
-			$itemManager 	= new ItemManager($db);
+			$item = $itemManager -> readById($_GET['id']);
+		}
+		catch (Exception $e)
+		{
+			$errors[] = $e -> getMessage();
+		}
 
 
-			// Get item
+		// Find existing basket or create new one
+		try
+		{
+			$order = $orderManager -> readByUser($currentUser, 0);
+		}
+		catch (Exception $e)
+		{
 			try
 			{
-				$item = $itemManager -> readById($_GET['id']);
+				$order = $orderManager -> create($currentUser);
 			}
 			catch (Exception $e)
 			{
 				$errors[] = $e -> getMessage();
 			}
+		}
 
 
-			// Find existing basket or create new one
+		// Check quantity
+		if ($item -> getQuantity() < $qty)
+		{
+			$errors[] = 'We only have '.$item -> getQuantity().' of this item in stock';
+		}
+
+		if (empty($errors))
+		{
 			try
 			{
-				$order = $orderManager -> readByUser($currentUser, 0);
+				$order -> updateItem($item, $qty);
 			}
 			catch (Exception $e)
 			{
-				try
-				{
-					$order = $orderManager -> create($currentUser);
-				}
-				catch (Exception $e)
-				{
-					$errors[] = $e -> getMessage();
-				}
-			}
-
-
-			// Check quantity
-			if ($item -> getQuantity() < $qty)
-			{
-				$errors[] = 'We only have '.$item -> getQuantity().' of this item in stock';
-			}
-
-			if (empty($errors))
-			{
-				try
-				{
-					$order -> updateItem($item, $qty);
-				}
-				catch (Exception $e)
-				{
-					$errors[] = $e -> getMessage();
-				}
+				$errors[] = $e -> getMessage();
 			}
 		}
 	}
